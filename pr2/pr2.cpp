060efef62267dd1d5242c547d5dd76c7e908f6f9
Александр Шумкин <shumkinsasha2005@mail.ru>
2023-10-05 10:15:43 +0300

    fix

--- pr2/pr2.cpp
+++ pr2/pr2.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -16,7 +17,11 @@ int main()
 
     double x, y, z, res1, res2, res3, res;
 
+    // НУЖНО ВВОДИТЬ ЗНАЧЕНИЯ ЧЕРЕЗ ЗАПЯТУЮ!!!
+ 
+
     //-15,246 0,04642 2000,1
+    //-25,4 576 -0,00475
 
     printf("Введите x, y, z> ");
     if (scanf_s("%lf %lf %lf", &x, &y, &z) != 3) {
@@ -35,7 +40,7 @@ int main()
     res = res1 * res2 + res3;
     printf("%.3f", res);
 
-    
+
 
     return 0;
 }
