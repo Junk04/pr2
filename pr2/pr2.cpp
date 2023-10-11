695886e706b8c93be00ff53c7adc8578e8a4d3e4
Александр Шумкин <shumkinsasha2005@mail.ru>
2023-10-11 19:32:19 +0300

    fixd

--- pr2/pr2.cpp
+++ pr2/pr2.cpp
@@ -5,32 +5,22 @@
 #include <windows.h>
 #include <iostream>
 
-
 using namespace std;
-
-
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
-    SetConsoleOutputCP(1251);
-
     double x, y, z, res1, res2, res3, res;
 
-    // НУЖНО ВВОДИТЬ ЗНАЧЕНИЯ ЧЕРЕЗ ЗАПЯТУЮ!!!
- 
+    //-15.246 0.04642 2000.1
+    //-25.4 576 -0.00475
 
-    //-15,246 0,04642 2000,1
-    //-25,4 576 -0,00475
-
-    printf("Введите x, y, z> ");
+    printf("x, y, z> ");
     if (scanf_s("%lf %lf %lf", &x, &y, &z) != 3) {
-        printf("Ошибка ввода.\n");
+        printf("Wrong.\n");
         return 1;
     }
 
     if (y <= 0) {
-        printf("Выражение по логарифмом меньше 0. Введите y > 0\n");
+        printf("The logarithm expression is less than 0. Enter y > 0\n");
         return 1;
     }
 
@@ -39,8 +29,6 @@ int main()
     res3 = pow(sin(atan(z)), 2);
     res = res1 * res2 + res3;
     printf("%.3f", res);
-
-
 
     return 0;
 }
